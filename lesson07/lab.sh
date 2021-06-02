f964959db52dd2f0155a119dedc3d2af120cc6a5
Sergey <sergey.kudelin@gmail.com>
2021-06-02 18:57:51 +0300

    feat: lesson07

---
+++ lesson07/lab.sh
@@ -0,0 +1,16 @@
+- wsadds
+```
+rsync -vaHP demo_folder root@192.168.10.11:/root
+```
+- moments:
+```
+- demo_folder/ vs demo_folder
+```
+- 
+```
+rsync -vaHP --remove demo_folder root@192.168.10.11:/root
+```
+- ddas
+```
+rsync -aHP --remove demo_folder root@192.168.10.11:/root
+```
