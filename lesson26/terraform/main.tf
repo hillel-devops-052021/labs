cb73fe6a09e81de96608419a4e1a0b12aa0e183c
Sergey <sergey.kudelin@gmail.com>
2021-08-15 08:37:31 +0300

    lesson27: jenkins_part01

---
+++ lesson26/terraform/main.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+locals {
+  user_data = <<EOF
+#!/bin/bash
+echo "Hello Terraform!"
+EOF
+}
+
+##################################################################
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "example"
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp", "all-icmp", "http-80-tcp", "https-443-tcp", "http-8080-tcp"]
+  egress_rules        = ["all-all"]
+}
+
+resource "aws_eip" "this" {
+  vpc      = true
+  instance = module.ec2.id[0]
+}
+
+resource "aws_kms_key" "this" {
+}
+
+resource "aws_network_interface" "this" {
+  count = 1
+
+  subnet_id = tolist(data.aws_subnet_ids.all.ids)[count.index]
+}
+
+module "ec2" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  instance_count = 1
+
+  name                        = "jenkins"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.medium"
+  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  associate_public_ip_address = true
+  key_name                    = "serhiikudelin"
+
+  user_data_base64 = base64encode(local.user_data)
+
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      volume_type = "gp2"
+      volume_size = 30
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
+
+  tags = {
+    "Env"     = "Develop"
+    "Purpose" = "jenkins"
+  }
+}
